e086fb03cb05d568f5f140d063a6a26d20b2e78c
Giang <tongtunggiang@gmail.com>
2020-01-23 12:36:30 +0000

    Camera Movement

---
+++ Assets/TowerDefenseECS/Code/Level/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BE
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraMovement : MonoBehaviour
+    {
+        public float m_CameraMoveSpeed;
+        public float m_CameraZoomSpeed;
+        public float m_MinHeight;
+        public float m_MaxHeight;
+
+
+        private void Update()
+        {
+            // Move
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            float movementRate = (transform.position.y - m_MinHeight) / (m_MaxHeight - m_MinHeight);
+            transform.position += movement.normalized * m_CameraMoveSpeed * Mathf.Abs(movementRate) * Time.deltaTime;
+
+            // Zoom
+            float scrollVal = Input.GetAxis("Mouse ScrollWheel");
+            if (transform.position.y >= m_MinHeight && transform.position.y <= m_MaxHeight ||
+                transform.position.y < m_MinHeight && scrollVal < 0 ||
+                transform.position.y > m_MaxHeight && scrollVal > 0)
+            {
+                transform.position += transform.forward * scrollVal * m_CameraZoomSpeed * Time.deltaTime;
+            }
+        }
+    }
+}
