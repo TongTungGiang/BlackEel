812cc5e5d7cd2f2c2d06d72f86d32a409494b9b2
Tong <K1929746@kingston.ac.uk>
2020-01-28 15:20:55 +0000

    Remove Experimental stuff

---
+++ Assets/TowerDefenseECS/Code/Level/AgentPrefabConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace BE.ECS
+{
+    public class AgentPrefabConverter : MonoBehaviour
+    {
+        public GameObject cube;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Entity prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(cube, World.Active);
+
+            GameData.Instance.RegisterAgentEntityPrefab(prefab);
+        }
+    }
+}
