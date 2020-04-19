00a6c62edbc9f8d688251ac7191e290839df2e12
Tống Tùng Giang <tongtunggiang@gmail.com>
2020-04-19 12:45:04 +0100

    Find attack target, with spatial partitioning!

--- Assets/TowerDefense/ECS/Code/ECS/Components/CellCoordinate.cs
+++ Assets/TowerDefense/ECS/Code/ECS/Components/CellCoordinate.cs
@@ -11,16 +11,10 @@ namespace BE.ECS
     public struct Cell : IEquatable<Cell>
     {
         public int x, y;
-        private const int CELL_SIZE = 10;
 
         public bool Equals(Cell other)
         {
             return other.x == x && other.y == y;
-        }
-
-        public static Cell FromPos(float3 position)
-        {
-            return new Cell { x = (int)position.x / CELL_SIZE, y = (int)position.z / CELL_SIZE };
         }
     }
 
--- Assets/TowerDefense/ECS/Code/ECS/Systems/FindAttackTargetSystem_SpatialPartioning.cs
+++ Assets/TowerDefense/ECS/Code/ECS/Systems/FindAttackTargetSystem_SpatialPartioning.cs
@@ -11,6 +11,7 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 using JacksonDunstan.NativeCollections;
+using Unity.Collections.LowLevel.Unsafe;
 
 namespace BE.ECS
 {
@@ -19,31 +20,59 @@ namespace BE.ECS
     {
         EntityQuery m_AllyQuery;
         EntityQuery m_EnemyQuery;
+        EntityCommandBufferSystem m_Barrier;
 
-        struct AddUniqueCellJob : IJobParallelFor
+
+        [BurstCompile]
+        private struct FindTargetInCell : IJobParallelFor
         {
-            [ReadOnly] public NativeArray<int> Input;
-            [WriteOnly] public NativeHashSet<int>.ParallelWriter OutputWriter;
+            [ReadOnly] public NativeMultiHashMap<int, FindAttackTargetDataTuple> InstigatorMap;
+            [ReadOnly] public NativeMultiHashMap<int, FindAttackTargetDataTuple> TargetMap;
+            [ReadOnly] public NativeArray<int> OverlappingCells;
+            [WriteOnly] public EntityCommandBuffer.Concurrent CommandBuffer;
+
+            [NativeSetThreadIndex] private int m_ThreadIndex;
 
             public void Execute(int index)
             {
-                OutputWriter.TryAdd(Input[index]);
+                int cellHash = OverlappingCells[index];
+
+                if (InstigatorMap.TryGetFirstValue(cellHash, out FindAttackTargetDataTuple currentInstigatorTuple, out NativeMultiHashMapIterator<int> instigatorIterator) == false)
+                    return;
+
+                do
+                {
+                    if (TargetMap.TryGetFirstValue(cellHash, out FindAttackTargetDataTuple currentTargetTuple, out NativeMultiHashMapIterator<int> targetIterator) == false)
+                        return;
+
+                    do
+                    {
+                        if (CheckInRange(currentTargetTuple.Position, currentInstigatorTuple.Position, currentInstigatorTuple.RangeSqr))
+                        {
+                            var attackTargetComponent = new AttackTargetComponent { Target = currentTargetTuple.Entity };
+                            CommandBuffer.AddComponent(m_ThreadIndex, currentInstigatorTuple.Entity, attackTargetComponent);
+
+                            //CommandBuffer.AddComponent(chunkIndex, EntityToTestAgainst[j], new OccupiedAsTargetTag());
+                            break;
+                        }
+                    } while (InstigatorMap.TryGetNextValue(out currentTargetTuple, ref targetIterator) == true);
+                } while (InstigatorMap.TryGetNextValue(out currentInstigatorTuple, ref instigatorIterator) == true);
+            }
+
+            private bool CheckInRange(float3 target, float3 center, float radiusSqr)
+            {
+                float3 delta = target - center;
+                float distanceSquare = delta.x * delta.x + delta.z * delta.z;
+
+                return distanceSquare <= radiusSqr;
             }
         }
 
-        struct FindTargetInCell : IJobParallelFor
+        private struct FindAttackTargetDataTuple
         {
-            [ReadOnly] public NativeMultiHashMap<int, Entity> InstigatorMap;
-            [ReadOnly] public NativeMultiHashMap<int, Entity> TargetMap;
-            [ReadOnly] public NativeArray<int> UniqueCells;
-
-            public void Execute(int index)
-            {
-                int cellHash = UniqueCells[index];
-
-                NativeMultiHashMapIterator<int> InstigatorIterator;
-                NativeMultiHashMapIterator<int> TargetIterator;
-            }
+            public Entity Entity;
+            public float3 Position;
+            public float RangeSqr;
         }
 
         protected override void OnUpdate()
@@ -51,28 +80,26 @@ namespace BE.ECS
             // Step 1.
             // Allocate entities into cell hashmap
             int allyCount = m_AllyQuery.CalculateEntityCount();
-            var allyMap = new NativeMultiHashMap<int, Entity>(allyCount, Allocator.TempJob);
+            var allyMap = new NativeMultiHashMap<int, FindAttackTargetDataTuple>(allyCount, Allocator.TempJob);
             var allyParallelMap = allyMap.AsParallelWriter();
             var allyJobHandle = Entities.WithAll<AgentTag, AllyTeamComponent>()
+                .WithNone<AttackTargetComponent>()
                 .WithName("CellAllocatingJob_Ally")
-                .ForEach((Entity e, in Translation t) =>
+                .ForEach((Entity e, in Translation t, in AttackRadiusComponent a) =>
                 {
-                    Cell c = Cell.FromPos(t.Value);
-                    var hash = (int)math.hash(new int2(c.x, c.y));
-                    allyParallelMap.Add(hash, e);
+                    allyParallelMap.Add(GetCellHash(in t), new FindAttackTargetDataTuple { Entity = e, Position = t.Value, RangeSqr = a.Value * a.Value });
                 })
                 .ScheduleParallel(Dependency);
 
             int enemyCount = m_EnemyQuery.CalculateEntityCount();
-            var enemyMap = new NativeMultiHashMap<int, Entity>(enemyCount, Allocator.TempJob);
+            var enemyMap = new NativeMultiHashMap<int, FindAttackTargetDataTuple>(enemyCount, Allocator.TempJob);
             var enemyParallelMap = enemyMap.AsParallelWriter();
             var enemyJobHandle = Entities.WithAll<AgentTag, EnemyTeamComponent>()
+                .WithNone<AttackTargetComponent>()
                 .WithName("CellAllocatingJob_Enemy")
-                .ForEach((Entity e, in Translation t) =>
+                .ForEach((Entity e, in Translation t, in AttackRadiusComponent a) =>
                 {
-                    Cell c = Cell.FromPos(t.Value);
-                    var hash = (int)math.hash(new int2(c.x, c.y));
-                    enemyParallelMap.Add(hash, e);
+                    enemyParallelMap.Add(GetCellHash(in t), new FindAttackTargetDataTuple { Entity = e, Position = t.Value, RangeSqr = a.Value * a.Value });
                 })
                 .ScheduleParallel(Dependency);
 
@@ -81,40 +108,71 @@ namespace BE.ECS
             Dependency = allocateCellJobHandle;
 
             // Step 2.
-            // Get list of unique cells
+            // Get the list of overlapping cells
+            // Note: using a quick O(n) algorithm to get overlapping elements of two array 
+            // because probably we don't have more than 100 cells 
+            // so no need to waste time on scheduling and waiting.
+            // A simple single-threaded piece of code might do the job
             var allyCellHashes = allyMap.GetKeyArray(Allocator.TempJob);
             var enemyCellHashes = enemyMap.GetKeyArray(Allocator.TempJob);
-            var uniqueCellHashes = new NativeHashSet<int>(allyCellHashes.Length + enemyCellHashes.Length, Allocator.TempJob);
-            var uniqueCellHashesWriter = uniqueCellHashes.AsParallelWriter();
-            var writeToUniqueCellJob_Ally = new AddUniqueCellJob { Input = allyCellHashes, OutputWriter = uniqueCellHashesWriter };
-            var writeToUniqueCellJob_Enemy = new AddUniqueCellJob { Input = enemyCellHashes, OutputWriter = uniqueCellHashesWriter };
+            var existingAllyHashes = new NativeHashSet<int>(allyCellHashes.Length + enemyCellHashes.Length, Allocator.TempJob);
+            for (int i = 0; i < allyCellHashes.Length; i++)
+            {
+                existingAllyHashes.TryAdd(allyCellHashes[i]);
+            }
 
-            var writeToUniqueCellJobHandle_Ally = writeToUniqueCellJob_Ally.Schedule(allyCellHashes.Length, 64);
-            writeToUniqueCellJobHandle_Ally.Complete();
-            var writeToUniqueCellJobHandle_Enemy = writeToUniqueCellJob_Enemy.Schedule(enemyCellHashes.Length, 64);
-            writeToUniqueCellJobHandle_Enemy.Complete();
+            var overlappingHashes = new NativeHashSet<int>(allyCellHashes.Length + enemyCellHashes.Length, Allocator.TempJob);
+            for (int i = 0; i < enemyCellHashes.Length; i++)
+            {
+                if (existingAllyHashes.Contains(enemyCellHashes[i]))
+                    overlappingHashes.TryAdd(enemyCellHashes[i]);
+            }
 
-            var uniqueCellHashesArray = uniqueCellHashes.ToNativeArray();
-
-            var writeToUniqueCellJobHandle = JobHandle.CombineDependencies(Dependency, writeToUniqueCellJobHandle_Ally);
-            writeToUniqueCellJobHandle = JobHandle.CombineDependencies(Dependency, writeToUniqueCellJobHandle_Enemy);
-            Dependency = writeToUniqueCellJobHandle;
+            var overlappingHashesArray = overlappingHashes.ToNativeArray();
+            existingAllyHashes.Dispose();
 
             // Step 3.
             // Perform target detection (O(n^2)) in allocated cells
+            if (overlappingHashes.Length > 0)
+            {
+                var commandBuffer = m_Barrier.CreateCommandBuffer().ToConcurrent();
+                var findTargetJob_Ally = new FindTargetInCell
+                {
+                    InstigatorMap = allyMap,
+                    TargetMap = enemyMap,
+                    OverlappingCells = overlappingHashesArray,
+                    CommandBuffer = commandBuffer,
+                };
 
+                var findTargetJob_Enemy = new FindTargetInCell
+                {
+                    InstigatorMap = enemyMap,
+                    TargetMap = allyMap,
+                    OverlappingCells = overlappingHashesArray,
+                    CommandBuffer = commandBuffer,
+                };
+
+                var findTargetJobHandle_Ally = findTargetJob_Ally.Schedule(overlappingHashes.Length, 64, Dependency);
+                findTargetJobHandle_Ally.Complete();
+                var findTargetJobHandle_Enemy = findTargetJob_Enemy.Schedule(overlappingHashes.Length, 64, Dependency);
+                findTargetJobHandle_Enemy.Complete();
+
+                var findTargetJobHandle = JobHandle.CombineDependencies(Dependency, findTargetJobHandle_Ally);
+                findTargetJobHandle = JobHandle.CombineDependencies(Dependency, findTargetJobHandle_Enemy);
+                Dependency = findTargetJobHandle;
+            }
 
             // Step 4.
             // Clean up
-            var uniqueCellHashesDisposeArrayJobHandle = uniqueCellHashesArray.Dispose(Dependency);
-            var uniqueCellHashesDisposeJobHandle = uniqueCellHashes.Dispose(Dependency);
+            var uniqueCellHashesArrayDisposeJobHandle = overlappingHashesArray.Dispose(Dependency);
+            var uniqueCellHashesDisposeJobHandle = overlappingHashes.Dispose(Dependency);
             var allyCellHashesDisposeJobHandle = allyCellHashes.Dispose(Dependency);
             var enemyCellHashesDisposeJobHandle = enemyCellHashes.Dispose(Dependency);
             var allyMapDisposeJobHandle = allyMap.Dispose(Dependency);
             var enemyMapDisposeJobHandle = enemyMap.Dispose(Dependency);
 
             var disposeJobHandle = Dependency;
-            disposeJobHandle = JobHandle.CombineDependencies(disposeJobHandle, uniqueCellHashesDisposeArrayJobHandle);
+            disposeJobHandle = JobHandle.CombineDependencies(disposeJobHandle, uniqueCellHashesArrayDisposeJobHandle);
             disposeJobHandle = JobHandle.CombineDependencies(disposeJobHandle, uniqueCellHashesDisposeJobHandle);
             disposeJobHandle = JobHandle.CombineDependencies(disposeJobHandle, allyCellHashesDisposeJobHandle);
             disposeJobHandle = JobHandle.CombineDependencies(disposeJobHandle, enemyCellHashesDisposeJobHandle);
@@ -125,26 +183,29 @@ namespace BE.ECS
 
         protected override void OnCreate()
         {
-            m_AllyQuery = GetEntityQuery(new EntityQueryDesc
-            {
-                All = new ComponentType[] { ComponentType.ReadOnly<AgentTag>(), ComponentType.ReadOnly<AllyTeamComponent>(), },
-            });
-
-            m_EnemyQuery = GetEntityQuery(new EntityQueryDesc
-            {
-                All = new ComponentType[] { ComponentType.ReadOnly<AgentTag>(), ComponentType.ReadOnly<EnemyTeamComponent>(), },
-            });
+            m_AllyQuery = GetAllAgent<AllyTeamComponent>();
+            m_EnemyQuery = GetAllAgent<EnemyTeamComponent>();
 
             RequireForUpdate(m_AllyQuery);
             RequireForUpdate(m_EnemyQuery);
+
+            m_Barrier = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
         }
 
-        private static bool CheckInRange(float3 target, float3 center, float radiusSqr)
+        EntityQuery GetAllAgent<TeamTag>() where TeamTag : struct
         {
-            float3 delta = target - center;
-            float distanceSquare = delta.x * delta.x + delta.z * delta.z;
-
-            return distanceSquare <= radiusSqr;
+            return GetEntityQuery(new EntityQueryDesc
+            {
+                All = new ComponentType[] { ComponentType.ReadOnly<AgentTag>(), ComponentType.ReadOnly<TeamTag>(), },
+            });
         }
+
+        private static int GetCellHash(in Translation t)
+        {
+            Cell c = new Cell { x = (int)t.Value.x / CELL_SIZE, y = (int)t.Value.z / CELL_SIZE };
+            return (int)math.hash(new int2(c.x, c.y));
+        }
+
+        private const int CELL_SIZE = 50;
     }
 }
